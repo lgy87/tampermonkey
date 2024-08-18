16e540b6e8f6d6b073a5e901cf4cdeeb0e78fd38
Guangyao Li <lgy87@foxmail.com>
2024-08-18 23:45:59 +0800

    fix: Cannot access 'id' before initialization

--- src/createRoot.js
+++ src/createRoot.js
@@ -1,6 +1,6 @@
-const id = "react-root-container__"
+const defaultId = "react-root-container__"
 
-const createRoot = (tag = "div", id = id) => {
+const createRoot = (tag = "div", id = defaultId) => {
   const exists = document.getElementById(id)
   if (exists) return exists
 
