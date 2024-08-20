87a17cebad189b70bd0306cf95c82754325ad88c
Guangyao Li <lgy87@foxmail.com>
2024-08-20 22:42:21 +0800

    feat: createRoot support specified style

--- src/createRoot.js
+++ src/createRoot.js
@@ -1,11 +1,25 @@
 globalThis.rapeflower ??= {}
 rapeflower.defaultId = "react-root-container__"
-globalThis.rapeflower.createRoot = (tag = "div", id = rapeflower.defaultId) => {
-  const exists = document.getElementById(id)
-  if (exists) return exists
+rapeflower.createRoot = (() => {
+  const normalize = key => key.replace(/([A-Z])/g, x => "-" + x.toLowerCase())
 
-  const element = document.createElement(tag)
-  element.id = id
-  document.body.appendChild(element)
-  return element
-}
+  const addStyleTo = (element, style) => {
+    if (!Object.keys(style).length) return element
+    Object.keys(style).forEach(key => {
+      element.style[normalize(key)] = style[key]
+    })
+    return element
+  }
+  return (tag = "div", id = rapeflower.defaultId, props = {}) => {
+    const exists = document.getElementById(id)
+    if (exists) return exists
+
+    const element = document.createElement(tag)
+    element.id = id
+    props.className && (element.className = props.className)
+    addStyleTo(element, props.style)
+
+    document.body.appendChild(element)
+    return element
+  }
+})()
