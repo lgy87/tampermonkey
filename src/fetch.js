c6613f93dee0085b43db2006d61147632f3da251
Guangyao Li <lgy87@foxmail.com>
2025-01-09 00:35:58 +0800

    feat: fetch helper

---
+++ src/fetch.js
@@ -0,0 +1,24 @@
+function fetch({
+  url,
+  method = "GET",
+}) {
+  return new Promise((resolve, reject) => {
+    GM_xmlhttpRequest({
+      url,
+      method: method.toUpperCase(),
+      onload(response) {
+        if (success(response)) {
+          return resolve(response.responseText)
+        }
+        reject(response.responseText)
+      },
+      onerror(error) {
+        reject(error)
+      }
+    })
+  })
+
+  function success(response) {
+    return response.status == 200
+  }
+}
