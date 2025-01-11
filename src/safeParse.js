b738b8c0d67f9eb6f28eaa1061acfc288ca0c1db
Guangyao Li <lgy87@foxmail.com>
2025-01-11 19:04:12 +0800

    fix: use undefined variable

--- src/safeParse.js
+++ src/safeParse.js
@@ -1,7 +1,7 @@
 globalThis.rapeflower ??= {};
 globalThis.rapeflower.safeParse = (json, defaults) => {
   try {
-    if (isNil(value)) return defaults;
+    if (isNil(json)) return defaults;
     return JSON.parse(json || "null");
   } catch (e) {
     console.error(e?.message || e);
