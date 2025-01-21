1fd8b65783fd591a620844ab0b858907c268fdba
Guangyao Li <lgy87@foxmail.com>
2025-01-21 16:20:44 +0800

    feat: uuid

---
+++ src/uuid.js
@@ -0,0 +1,19 @@
+globalThis.rapeflower ??= {};
+globalThis.rapeflower.uuid = {
+  value(key) {
+    const { local } = rapeflower || {}
+    const uuidKey = "rapeflower-uuid"
+
+    const cache = local.read(uuidKey, {})
+    if (isNil(cache[key])) cache[key] = 0
+   
+    if (cache[key] >= Number.MAX_SAFE_INTEGER - 10000) cache[key] = 0
+    cache[key]++
+    local.save(uuidKey, cache)
+    return cache[key]
+  },
+};
+
+function isNil(value) {
+  return value == null
+}
