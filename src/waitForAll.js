c3f9736ff47ec1db19d8b3bf1d8e6ea75616fc11
Guangyao Li <lgy87@foxmail.com>
2025-01-22 13:33:47 +0800

    feat: waitForAll 2

--- src/waitForAll.js
+++ src/waitForAll.js
@@ -3,20 +3,8 @@ globalThis.rapeflower.waitForAll = (selectors, {
   interval = 50,
   maxTry = 100,
   predicate = $element => $element?.length > 0,
-}) => new Promise((resolve, reject) => {
-  let count = 0
-  const f = () => {
-    const $elements = selectors.map(selector => $(selector))
-    if ($elements.every(predicate)) {
-      return resolve($elements)
-    }
-
-    if (count++ > maxTry) {
-      console.log("can't find: ", selectors)
-      return reject()
-    }
-    setTimeout(f, interval)
-  }
-  f()
-})
+}) => {
+  const { waitFor } = globalThis.rapeflower
+  return Promise.all(selectors.map(selector => waitFor(selector, { interval, maxTry, predicate })))
+}
 
