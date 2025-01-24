a20cf8fb364465fddc9bfa0b1d450b92b9a048f8
Guangyao Li <lgy87@foxmail.com>
2025-01-24 19:48:44 +0800

    feat: useIcons

---
+++ src/useIcons.js
@@ -0,0 +1,7 @@
+globalThis.rapeflower ??= {}
+globalThis.rapeflower.useIcons = () => {
+  rapeflower.attachStyleFile(
+    "https://unpkg.com/@phosphor-icons/web@2.1.1/src/fill/style.css",
+  )
+}
+
