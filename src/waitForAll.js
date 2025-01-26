7733d5e1de6ddd6c2cc36e31e55d1bab6452e2e4
Guangyao Li <lgy87@foxmail.com>
2025-01-26 10:48:15 +0800

    fix: waitForAll defaults parameters

--- src/waitForAll.js
+++ src/waitForAll.js
@@ -3,7 +3,7 @@ globalThis.rapeflower.waitForAll = (selectors, {
   interval = 50,
   maxTry = 100,
   predicate = $element => $element?.length > 0,
-}) => {
+} = {}) => {
   const { waitFor } = globalThis.rapeflower
   return Promise.all(selectors.map(selector => waitFor(selector, { interval, maxTry, predicate })))
 }
