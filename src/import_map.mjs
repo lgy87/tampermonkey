7dd1a0f212d1909ffe2a9da9712328178880b073
Guangyao Li <lgy87@foxmail.com>
2026-03-31 01:02:01 +0800

    feat: change jsdelivr to cdn.statically.io

--- src/import_map.mjs
+++ src/import_map.mjs
@@ -2,7 +2,7 @@ const map = document.createElement("script")
 map.type = "importmap"
 map.textContent = JSON.stringify({
   imports: {
-    "@lgy87/": "https://cdn.jsdelivr.net/gh/lgy87/tampermonkey@esm/src/",
+    "@lgy87/": "https://cdn.statically.io/gh/lgy87/tampermonkey@esm/src/",
   },
 })
 document.head.appendChild(map)
