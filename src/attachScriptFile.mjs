1fdd937e1a5da3b265e78c61ccf39016284404cc
Guangyao Li <lgy87@foxmail.com>
2026-04-01 00:24:39 +0800

    feat: optimize attachScriptFile.mjs

--- src/attachScriptFile.mjs
+++ src/attachScriptFile.mjs
@@ -1,6 +1,7 @@
 export function attachScriptFile(url) {
   const script = document.createElement("script")
   script.type = "module"
+  script.crossOrigin = "anonymous"
   script.src = url
 
   document.head.appendChild(script)
