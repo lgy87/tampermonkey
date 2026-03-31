49c4e1546ba96e5f2e942886b04c9c3b46f73fac
Guangyao Li <lgy87@foxmail.com>
2026-03-31 19:09:10 +0800

    feat: optimize importMap

--- src/importMap.mjs
+++ src/importMap.mjs
@@ -1,16 +1,34 @@
 const rapeflower = {
-  init(name = "@lgy87", tag = "esm") {
+  cached: {
+    "@esm/": "https://esm.sh/",
+    "@lgy87/": "https://cdn.jsdelivr.net/gh/lgy87/tampermonkey@esm/src/",
+  },
+  cache(key, url) {
+    if (this.cached[key]) return
+
+    this.cached[key] = url
+  },
+  init() {
+    const map = document.createElement("script")
+    map.type = "importmap"
+    map.textContent = JSON.stringify({ imports: this.cached })
+    document.head.appendChild(map)
+  },
+  add(name, tag) {
+    if (!name || !tag) return
+
+    const key = `${name}/`
+    const url = `https://cdn.jsdelivr.net/gh/lgy87/tampermonkey@${tag}/src/`
+    this.cache(key, url)
+
     const map = document.createElement("script")
     map.type = "importmap"
     map.textContent = JSON.stringify({
-      imports: {
-        "@esm/": "https://esm.sh/",
-        [`${name}/`]: `https://cdn.jsdelivr.net/gh/lgy87/tampermonkey@${tag}/src/`,
-      },
+      imports: { [key]: url },
     })
     document.head.appendChild(map)
   },
 }
 
 rapeflower.init()
-globalThis.initImportMap = rapeflower.init
+globalThis.addImportMap = rapeflower.add
