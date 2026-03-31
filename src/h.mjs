8a4efce5df044498844df9588a3f6d65c2ea5ca5
Guangyao Li <lgy87@foxmail.com>
2026-03-31 20:07:48 +0800

    feat: remove h.mjs

--- src/h.mjs
+++
@@ -1,8 +0,0 @@
-import { emptyArray } from "./emptyArray.mjs"
-import { emptyObject } from "./emptyObject.mjs"
-import { ensureArray } from "./ensureArray.mjs"
-
-export function h(tag, { children = emptyArray, ...rest } = emptyObject) {
-  const ensuredArray = ensureArray(children)
-  return React.createElement(tag, rest, ...ensuredArray)
-}
